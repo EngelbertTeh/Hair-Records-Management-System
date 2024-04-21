5937e8aa8c0b6997476f763c378249601a98e52c
engelbert teh <engelbertteh.dev@gmail.com>
2024-04-21 11:07:21 +0800

    added url validation to links form

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,3 +1,4 @@
+import isValidURL from '@/app/lib/isValidURL';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
@@ -13,5 +14,15 @@ export async function POST(req: NextRequest) {
   }
 
   const data = await req.json();
+  const url = data && data.url ? data.url : null;
+  console.log(url);
+  const isValid =
+    url && (await isValidURL(url, [process.env.NEXT_PUBLIC_VERCEL_ENV!]));
+  if (!isValid) {
+    return NextResponse.json(
+      { message: `${url} is not valid` },
+      { status: 400 }
+    );
+  }
   return NextResponse.json(data, { status: 201 });
 }
--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,9 +1,9 @@
-import LinksCreateHTMLForm from './createHTMLForm';
+import LinksCreateForm from './createForm';
 
 export default function LinksPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LinksCreateHTMLForm />
+      <LinksCreateForm />
     </main>
   );
 }
