302ab2726a853bd915733f0a2eb1b0569576b114
engelbert teh <engelbertteh.dev@gmail.com>
2024-05-02 10:14:28 +0800

    added shadcn carousel

--- src/components/card/AppointmentCard/index.tsx
+++ src/components/card/AppointmentCard/index.tsx
@@ -2,13 +2,25 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 
 const Index = () => {
   return (
-    <ScrollArea className="h-[200px] w-[350px] rounded-md border-x-1 px-4">
-      Jokester began sneaking into the castle in the middle of the night and
-      leaving jokes all over the place: under the kings pillow, in his soup,
-      even in the royal toilet. The king was furious, but he couldnt seem to
-      stop Jokester. And then, one day, the people of the kingdom discovered
-      that the jokes left by Jokester were so funny that they couldnt help but
-      laugh. And once they started laughing, they couldnt stop.
+    <ScrollArea className="h-[200px] w-[350px]  border-x-1 mx-0 px-0">
+      <div className="my-[0.5px] ">
+        Jokester began sneaking into the castle in the middle of the night and
+      </div>
+      <div className="my-[0.5px] ">
+        leaving jokes all over the place: under the kings pillow, in his soup,
+      </div>
+      <div className="my-[0.5px] ">
+        even in the royal toilet. The king was furious, but he couldnt seem to
+      </div>
+      <div className="my-[0.5px] ">
+        stop Jokester. And then, one day, the people of the kingdom discovered
+      </div>
+      <div className="my-[0.5px] ">
+        that the jokes left by Jokester were so funny that they couldnt help but
+      </div>
+      <div className="my-[0.5px] ">
+        laugh. And once they started laughing, they couldnt stop.
+      </div>
     </ScrollArea>
     // <Skeleton className= "w-[100px] h-[20px] rounded-full" />
   );
---
+++ src/components/card/ServiceDetail/index.tsx
@@ -0,0 +1,17 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+<Carousel>
+  <CarouselContent>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>;
