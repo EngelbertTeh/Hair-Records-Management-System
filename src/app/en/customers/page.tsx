e6657277d41ec7132676b29b40669b1f8682a36c
engelbert teh <engelbertteh.dev@gmail.com>
2024-05-05 15:27:10 +0800

    added shadow for nth child

--- src/app/en/customers/page.tsx
+++ src/app/en/customers/page.tsx
@@ -4,23 +4,21 @@ export default function CustomerList() {
   return (
     <div className="card-container parent gr3">
       <div className="card-container fc row-span-1">
-        <i className="bx bx-search" style={{ color: '#ff0000' }}></i>
-        <i className="bx bx-search"></i>
         <Card className="panel">
           <CardHeader>
             <CardTitle className="title ">RIMIX</CardTitle>
           </CardHeader>
         </Card>
-        <Card className="panel fc flex-auto">
+        <Card className="panel fc flex-auto ">
           <CardHeader>
             <CardTitle className="header ">CUSTOMERS</CardTitle>
           </CardHeader>
-          <CardContent className="flex  justify-center">
+          <CardContent className="flex justify-center">
             <SearchBar className="searchbar" />
           </CardContent>
         </Card>
       </div>
-      <div className=" card-container row-span-2">
+      <div className="card-container ">
         <Card className="panel min-h-full">Somebody</Card>
       </div>
     </div>
